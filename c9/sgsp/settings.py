85d325513066d466e1b7f04a0fd2c2ad867a9498
stevenshuang <stevenshuang521@gmail.com>
2018-05-29 22:23:30 +0800

    settings file

---
+++ c9/sgsp/settings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_PASSWORD = None
+REDIS_KEY = 'sg'
+
+MYSQL_HOST = 'localhost'
+MYSQL_PORT = 3306
+MYSQL_PASSWORD = '*********'
+MYSQL_USER = 'root'
+MYSQL_DATABASE = 'sg_wx'
+
+MAX_FAIL_TIME = 10
+TIMEOUT = 10
