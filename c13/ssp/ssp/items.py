17d9c39136017e504424ddd6e3bfa3860178ac8e
stevenshuang <stevenshuang521@gmail.com>
2018-06-12 19:02:46 +0800

    定义商品的属性

---
+++ c13/ssp/ssp/items.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy import Field
+
+
+class SspItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    
+    collection = 'products'
+    image = Field()
+    price = Field()
+    deal = Field()
+    title = Field()
+    shop = Field()
+    location = Field()
+
