4bee8c826382432d96e11c9497906f3a117d9a0d
stevenshuang <stevenshuang521@gmail.com>
2018-06-13 22:49:24 +0800

    将通用的爬虫的Rule提取

---
+++ c13/china/china/rules.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import Rule
+
+
+rules = {
+    'china': (
+        Rule(LinkExtractor(allow='article\/.*\.html', 
+            restrict_xpaths='//div[@id="left_side"]//div[@class="con_item"]'), 
+            callback='parse_item'),
+        Rule(LinkExtractor(restrict_xpaths='//div[@id="pageStyle"]/a[contains(., "下一页")]'))
+    ),
+}
