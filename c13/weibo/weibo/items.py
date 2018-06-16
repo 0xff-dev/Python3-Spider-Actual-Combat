50942262ffe176f0a53f3bce28b1f6121d4c738e
stevenshuang <stevenshuang521@gmail.com>
2018-06-16 23:30:00 +0800

    微博的存储对象

---
+++ c13/weibo/weibo/items.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy import Field
+
+
+class WeiboItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+
+class UserItem(scrapy.Item):
+    collection = 'users'
+    id = Field()
+    name = Field()
+    avatar = Field()
+    cover = Field()
+    gender = Field()
+    description = Field()
+    fans_count = Field()
+    follows_count = Field()
+    weibos_count = Field()
+    verified = Field()
+    verified_reason = Field()
+    verified_type = Field()
+    follows = Field()
+    fans = Field()
+    crawled_at = Field()
+
+
+class UserRelationItem(scrapy.Item):
+    collection = 'users'
+    id = Field()
+    follows = Field()
+    fans = Field()
+
+
+class WeiboItem(scrapy.Item):
+    collection = 'weibos'
+    id = Field()
+    attitudes = Field()
+    comments_count = Field()
+    reposts_count = Field()
+    picture = Field()
+    pictures = Field()
+    source = Field()
+    text = Field()
+    raw_text = Field()
+    thumbnail = Field()
+    user = Field()
+    created_at = Field()
+    crawled_at = Field()
+
+
+
